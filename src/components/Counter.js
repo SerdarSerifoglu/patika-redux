a3aec92e94ef11b7b7b65ae467b77ada670f0df0
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-01-03 16:10:32 +0300

    reducer'lar eklendi. reducer'a parametre geçilen bir örnekte yapıldı.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,23 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, incrementByAmount } from "../redux/counter/counterSlice";
+
 
 const Counter = () => {
+    const [amount, setAmount] = useState(3);
     const countValue = useSelector((x) => x.counter.value);
+    const dispatch = useDispatch();
 
     return (
         <div>
             <h1>SERDAR: {countValue}</h1>
+
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+
+            <br></br>
+            <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}></input>
+            <button onClick={() => dispatch(incrementByAmount(amount))}>Increment By Amount</button>
         </div>
     )
 }
