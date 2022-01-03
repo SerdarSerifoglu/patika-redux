69911ef8d462538c20d7e010b58229a812b7e972
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-01-03 21:54:54 +0300

    todo-app için mevcut html'ler codependen react'a component olarak geçirildi

---
+++ todo-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form.js";
+
+const Header = () => {
+    return(
+        <header className="header">
+		<h1>todos</h1>
+		<Form></Form>
+	    </header>
+	
+    )
+}
+
+export default Header;
