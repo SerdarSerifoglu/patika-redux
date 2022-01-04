dc1a3426d07763faefe67afdcb9bf4c05c6ec0b1
SerdarSerifoglu <32906871+SerdarSerifoglu@users.noreply.github.com>
2022-01-04 12:47:04 +0300

    state'teki todos datasına ekleme işlemi yapıldı

--- todo-app/src/redux/todos/todosSlice.js
+++ todo-app/src/redux/todos/todosSlice.js
@@ -13,12 +13,16 @@ const initialState = {
     ]
 };
 
+//redux toolkit createSlice içerisinde immerJs kütüphanesini kullanarak state'i klonlamaya gerek kalmadan düzenleme yapmamızı sağlar.
 const todosSlice = createSlice({
     name: "todos",
     initialState: initialState,
     reducers: {
-
+        addTodo: (state, action) => {
+            state.items.push(action.payload);
+        }
     }
 })
 
+export const { addTodo } = todosSlice.actions;
 export default todosSlice.reducer;
